425bd308b30ac8907968e2a12bb45be3e90536ea
Pasha <justuser166@gmail.com>
2023-04-23 20:52:48 +0300

    first commit

---
+++ cart/cartapp/views.py
@@ -0,0 +1,31 @@
+from django.shortcuts import render
+from django.http import JsonResponse
+from cartapp.models import *
+# Create your views here.
+def show_products(request):
+    response = render(request,"products.html",context={"products": Product.objects.all()})
+    if request.method == "POST":
+        if "product_pk" not in request.COOKIES: #
+            new_product = request.POST.get('product_pk') #
+            response.set_cookie('product_pk',new_product) #
+            return response #
+        else: #
+            new_product = request.COOKIES['product_pk'] + " " + request.POST.get('product_pk') # "1" + " " + "2"  = "1 2"
+            response.set_cookie('product_pk',new_product) #
+            return response #
+
+    return response
+
+def show_cart(request):
+    if "product_pk" in request.COOKIES: #
+        
+        
+        products_pk = products_pk.split(' ')
+
+        list_products = list()
+        for product_pk in products_pk:
+            list_products.append(Product.objects.get(pk=product_pk))
+        response = render(request,"cart.html",context={"products": list_products})
+    else:
+        response = render(request,"cart.html",context={"products":list()})
+    return response
